8c15c49cbf36be9e5cdcaab6c6e79f0ad5215433
OtabekValiboyev <valiboyevo@gmail.com>
2023-12-02 20:25:13 +0500

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-// create-item-inner qismi create item top qismi bosilganida togler bo`lishi kerak.
 let modifiers = {
-  elItemOpen: 'create-item-top--open'
-}
+  elItemOpen: "create-item-top--open",
+  elCreateLinkActive: "crate-link-active",
+};
 
-
-const elsCreateItemTop = document.querySelectorAll('.js-create-item-top');
-const elsCreateItemTogler = document.querySelectorAll('.js-create-item-togler');
-
-// console.log(elsCreateItemTop);
-// console.log(elsCreateItemTogler);
-
-elsCreateItemTogler.forEach(function (elCreateItemTogler) {
-  elCreateItemTogler.addEventListener('click', function () {
-    elsCreateItemTop.forEach(function (elCreateItemTop) {
-      elCreateItemTop.classList.toggle(modifiers.elItemOpen);
-    })
+const elsCreateItemTop = document.querySelectorAll(".js-create-item-top");
+elsCreateItemTop.forEach(function (item) {
+  item.addEventListener("click", () => {
+    item.classList.toggle(modifiers.elItemOpen);
   });
 });
-
-//create-item-inner qismining ichidagi boxlar bosilganda active holatga, qayta bosilganida
